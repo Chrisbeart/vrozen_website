061d143a96e578c5cad2a01aed6b655229bd5b0a
Chris Breunig <chrisbreunig2311@gmail.com>
2024-05-26 14:22:41 +0200

    TitelBanner eingefügt

--- vrozen_website/src/components/Banner.js
+++ vrozen_website/src/components/Banner.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Banner.css';
+import bannerImage from '../assets/images/banner.jpg';  // Pfad zum Bild
 
 const Banner = () => {
   return (
-    <div className="banner">
+    <div className="banner" style={{ backgroundImage: `url(${bannerImage})` }}>
       <h1>Title Banner</h1>
       <p>Some Name</p>
     </div>
