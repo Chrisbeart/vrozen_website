a2b09e5b24073b27f20b5f5e38465da52922a82d
Chris Breunig <chrisbreunig2311@gmail.com>
2024-05-26 23:40:02 +0200

    navbar transparent

--- vrozen_website/src/components/Navbar.js
+++ vrozen_website/src/components/Navbar.js
@@ -37,8 +37,7 @@ const MyNavbar = () => {
   }, []);
 
   return (
-    <Navbar fixed="top" bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/">Brand</Navbar.Brand>
+    <Navbar fixed="top" className="navbar-transparent" expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
