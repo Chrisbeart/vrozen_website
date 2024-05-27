5e9f3d60704dfbe296cfeca106954c6d11bbfd42
BrainsenOv3erkill <165007784+BrainsenOv3erkill@users.noreply.github.com>
2024-05-27 14:51:07 +0200

    Bilder in Pges

--- vrozen_website/src/pages/Bilder.js
+++ vrozen_website/src/pages/Bilder.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Bilder.css'
 
 const Bilder = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleImageClick = (index) => {
+    setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
+  const images = [
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    'https://www.ehorses.de/magazin/wp-content/uploads/mustang-pferderasse.jpg',
+    
+  ];
+
   return (
-    <div className="bilder">
-      <h1>Bilder</h1>
-      <Link to="/bilder-detail">Go to Bilder Detail</Link>
+    <div className="wrapper">
+      <div className="bilder">
+        <h1>Bilder</h1>
+        <Link to="/bilder-detail">Go to Bilder Detail</Link>
+        <div className="items">
+          {images.map((src, index) => (
+            <div
+              key={index}
+              className={`item ${activeIndex === index ? 'active' : ''}`}
+              style={{ backgroundImage: `url(${src})` }}
+              onClick={() => handleImageClick(index)}
+            ></div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
