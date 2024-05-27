4d6e992bd55f83b4dfb8a8c16810edb896ba5e1f
TuxUbuntu <henke.stefan1985@gmail.com>
2024-05-27 17:13:55 +0200

    04

--- vrozen_website/src/pages/Uebermich.js
+++ vrozen_website/src/pages/Uebermich.js
@@ -1,32 +1,14 @@
-// src/pages/Uebermich.js
+// src/pages/UeberMich.js
+
 import React from 'react';
-import  './Uebermich.css'; // Importiere die CSS-Datei
 
-
-const Uebermich = () => {
+const UeberMich = () => {
   return (
-    <div className="über-mich-container">
-      <h1>Über Mich</h1>
-      <div className="content">
-        <div className="image-container">
-          <img
-            src="https://via.placeholder.com/300"
-            alt="Profilbild"
-            className="profilbild"
-          />
-        </div>
-        <div className="text-container">
-          <p className="vorstellungstext">
-            Herzlich willkommen auf meiner Über-mich-Seite! Mein Name ist Max
-            Mustermann und ich bin ein begeisterter Entwickler aus Musterstadt.
-            Seit meiner Kindheit interessiere ich mich für das Programmieren und
-            habe in den letzten Jahren an vielen spannenden Projekten
-            mitgewirkt.
-          </p>
-        </div>
-      </div>
+    <div>
+      <h1>Über mich</h1>
+      <p>Hier können Sie Informationen über mich hinzufügen.</p>
     </div>
   );
 };
 
-export default Uebermich;
+export default UeberMich;
